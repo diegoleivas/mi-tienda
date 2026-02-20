4c7779ada06b834364578ed5316b5d06286f9f39
diegoleivas <leivasdiego@gmail.com>
2026-02-19 21:31:19 -0300

    se modifica db.js de backend

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,11 @@
-
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "diego",
-  password: "1234",
-  database: "tienda"
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "diego",
+  password: process.env.MYSQLPASSWORD || "1234",
+  database: process.env.MYSQLDATABASE || "tienda",
+  port: process.env.MYSQLPORT || 3306,
 });
 
-module.exports = connection;  
+module.exports = connection;
