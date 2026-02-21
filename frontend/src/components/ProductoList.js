924ad631cdfca0b50136e5f72feea721ff98fad3
diegoleivas <leivasdiego@gmail.com>
2026-02-21 02:42:16 -0300

    fix producto list imágenes

--- frontend/src/components/ProductoList.js
+++ frontend/src/components/ProductoList.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./ProductoList.css"; // 👈 archivo de estilos
+import "./ProductoList.css";
+
+const API_URL = "https://mi-tienda-9ku2.onrender.com";
 
 function ProductoList() {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/abm/productos", { withCredentials: true })
+    axios.get(`${API_URL}/abm/productos`, { withCredentials: true })
       .then((res) => {
         console.log("Respuesta backend:", res.data);
 
@@ -20,33 +22,34 @@ function ProductoList() {
   }, []);
 
   return (
-  <div className="listado-productos">
-    <ul className="lista-productos">
-      {productos.length > 0 ? (
-        productos.map((producto) => (
-          <li key={producto.id} className="item-producto">
-            <img
-              src={`/images/${producto.imagen}`}
-              alt={producto.nombre}
-              className="miniatura"
-            />
-            <div className="info-producto">
-              <strong>{producto.nombre}</strong>
-              <span>Precio: ${producto.precio}</span>
-              <span>Stock: {producto.stock}</span>
-              <span>Categoría: {producto.categoria}</span>
-              <p>{producto.descripcion}</p>
+    <div className="listado-productos">
+      <ul className="lista-productos">
+        {productos.length > 0 ? (
+          productos.map((producto) => (
+            <li key={producto.id} className="item-producto">
               
-            </div>
-          </li>
-        ))
-      ) : (
-        <li>No hay productos disponibles</li>
-      )}
-    </ul>
-  </div>
-);
+             <img
+               src={`${API_URL}${producto.imagen}`}
+               alt={producto.nombre}
+               className="miniatura"
+            />
 
+              <div className="info-producto">
+                <strong>{producto.nombre}</strong>
+                <span>Precio: ${producto.precio}</span>
+                <span>Stock: {producto.stock}</span>
+                <span>Categoría: {producto.categoria}</span>
+                <p>{producto.descripcion}</p>
+              </div>
+
+            </li>
+          ))
+        ) : (
+          <li>No hay productos disponibles</li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default ProductoList;
