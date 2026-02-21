6071c7ae48caedd7644eb22f922d98915a9a2630
diegoleivas <leivasdiego@gmail.com>
2026-02-21 00:31:20 -0300

    gg

--- backend/generarHash.js
+++ backend/generarHash.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 
 async function generarHash() {
-  const password = "27789318dal";
+  const password = "27789318Dal";
   const hash = await bcrypt.hash(password, 10);
   console.log("Hash generado:", hash);
 }
