8b2a01a9e4002f2e2686b20c1a0f745b584208b0
diegoleivas <leivasdiego@gmail.com>
2026-02-22 00:17:19 -0300

    fix footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <footer style={{ marginTop: "2rem", padding: "1rem", background: "#eee", textAlign: "center" }}>
-      <p>© 2026 Tienda Praxis - Todos los derechos reservados</p>
-      <p>Contacto: info@tiendapraxis.com</p>
+      <p>© 2026 Tienda Fileteando Ando  - Todos los derechos reservados</p>
+      <p>Contacto: leivasdiego@gmail.com</p>
     </footer>
   );
 }
