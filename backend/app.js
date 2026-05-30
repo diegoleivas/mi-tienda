9c20e0cf8105e26abf4fda644d8fa277bebef7a3
diegoleivas <leivasdiego@gmail.com>
2026-05-30 10:57:37 -0300

    se modifica la duracion de la sesio

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 30, // 30 min
+    // maxAge: 1000 * 60 * 30, // 30 min
+    maxAge: 1000 * 20, // 20 segundos para pruebas
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
   }
